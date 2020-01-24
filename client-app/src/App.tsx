9214313bdd415eab712a572d20dd3dcd65d8de26
Kheneahm Ares <kheneahm9@gmail.com>
2020-01-24 14:39:32 -0600

    adding react client

---
+++ client-app/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <ul>
+        
+      </ul>
+      
+    </div>
+  );
+}
+
+export default App;
